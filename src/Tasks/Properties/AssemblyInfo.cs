002ffba563c566cc890fbdc9a8068ace07c4fa1f
Daniel Cazzulino <daniel@cazzulino.com>
2014-06-20 01:17:58 -0300

    Add missing AssemblyInfo.cs for Tasks project.

---
+++ src/Tasks/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: System.Reflection.AssemblyTitle("Tasks")]
+[assembly: System.Reflection.AssemblyProduct("Tasks")]
